88c39b7d2a71ce4ad40121a6d5acdb2d22498905
Abhisahu22 <135636109+Abhisahu22@users.noreply.github.com>
2024-06-25 14:42:48 +0530

    Add files via upload

---
+++ resnet_1_data.py
@@ -0,0 +1,26 @@
+
+import torchvision
+import torchvision.transforms as transforms
+
+
+
+transform_train = transforms.Compose([
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomCrop(32, padding=4),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+])
+
+train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+
+
+
+test = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+
+
+print(len(train_dataset))
